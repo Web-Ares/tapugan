9dcadfc50b95fd01702a83eb1a1ff018e17a36d1
iversontb <petruninevgeniy@gmail.com>
2016-07-13 10:25:49 +0300

    menu done

--- app/js/app.js
+++ app/js/app.js
@@ -1,46 +1,4 @@
 $(function(){
     'use strict';
 
-
-    $(function(){
-
-        $('.menu').each( function() {
-            new Menu( $(this) );
-        } );
-
-    });
-
-    var Menu = function(obj) {
-
-        //private properties
-        var _obj = obj,
-            _btn = _obj.find( '.menu__btn' ),
-            _window = $(window);
-
-        //private methods
-        var _addEvents = function() {
-
-                _btn.on({
-                    'click': function() {
-
-                        if ( !_obj.hasClass( 'active' ) ) {
-                            _obj.addClass( 'active' );
-                        } else {
-                            _obj.removeClass( 'active' );
-                        }
-                    }
-                });
-
-            },
-            _init = function() {
-                _addEvents();
-            };
-
-        //public properties
-
-        //public methods
-
-        _init();
-    };
-
 });
