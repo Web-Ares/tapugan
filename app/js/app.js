163419cb0664dce8ec59fcdc0b269c1d55c1dda8
iversontb <petruninevgeniy@gmail.com>
2016-07-13 11:47:33 +0300

    swf animation done

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,36 @@
 $(function(){
     'use strict';
 
+
+    $(function() {
+
+        $('.site__ban_flash').each(function () {
+            new HomeFlash($(this));
+        });
+
+    });
+
+    var HomeFlash = function(obj) {
+
+        //private properties
+        var _obj = obj,
+            flashvars = {},
+            params = {},
+            attributes = {};;
+
+        //private methods
+        var _addEvents = function() {
+
+            },
+            _init = function() {
+                _addEvents();
+                swfobject.embedSWF('home.swf', "flash", "1525", "530", "10",false, flashvars, params, attributes);
+            };
+
+        //public properties
+
+        //public methods
+
+        _init();
+    };
 });
